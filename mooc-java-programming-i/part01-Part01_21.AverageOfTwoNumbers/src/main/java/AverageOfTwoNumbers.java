b00acacf29c28b3a55c48ee96d279b72818b4cd5
Jokieh <1stone.seres@gmail.com>
2022-05-26 02:18:06 +0200

    finished AverageOfTwoNumbers

--- mooc-java-programming-i/part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
+++ mooc-java-programming-i/part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
@@ -6,7 +6,15 @@ public class AverageOfTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        Integer first = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        Integer second = Integer.valueOf(scanner.nextLine());
+
+        Double average = 1.0 * (first + second) / 2;
+
+        System.out.println("The average is " + average);
 
     }
 }
