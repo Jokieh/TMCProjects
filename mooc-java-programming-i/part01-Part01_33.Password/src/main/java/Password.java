1ed007ed23881c2f22a12fdc6db56c2c7fecd393
Jokieh <1stone.seres@gmail.com>
2022-05-30 03:08:30 +0200

    finished Password

--- mooc-java-programming-i/part01-Part01_33.Password/src/main/java/Password.java
+++ mooc-java-programming-i/part01-Part01_33.Password/src/main/java/Password.java
@@ -6,6 +6,19 @@ public class Password {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here 
+        String password = "Caput Draconis";
+
+        System.out.println("Password ?");
+        String inputPassword = scan.nextLine();
+
+        if (password.equals(inputPassword)) {
+
+            System.out.println("Welcome!");
+
+        }   else {
+
+            System.out.println("Off with you!");
+
+        }
     }
 }
