96d256f7d6635e65343f0dc0760ddff4299f5e7f
Jokieh <1stone.seres@gmail.com>
2022-05-31 10:54:32 +0200

    finished OnlyPositives

--- mooc-java-programming-i/part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ mooc-java-programming-i/part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -6,5 +6,28 @@ public class OnlyPositives {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+
+            System.out.println("Give a number:");
+            Integer number = Integer.valueOf(scanner.nextLine());
+
+            if (number < 0) {
+
+                System.out.println("Unsuitable number");
+
+            } else if (number == 0) {
+
+                break;
+
+            } else if (number > 0) {
+
+                System.out.println(Math.pow(number, 2));
+
+            }
+
+
+        }
+
+
     }
 }
