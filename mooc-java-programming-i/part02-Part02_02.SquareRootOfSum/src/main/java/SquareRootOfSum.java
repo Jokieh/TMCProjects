e4bdc9ce125ef925c7e59b9c9e57a00e29389193
Jokieh <1stone.seres@gmail.com>
2022-05-31 10:31:19 +0200

    finished SquareRootOfSum

--- mooc-java-programming-i/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ mooc-java-programming-i/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -6,5 +6,9 @@ public class SquareRootOfSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Integer number1 = Integer.valueOf(scanner.nextLine());
+        Integer number2 = Integer.valueOf(scanner.nextLine());
+
+        System.out.println(Math.sqrt(number1 + number2));
     }
 }
