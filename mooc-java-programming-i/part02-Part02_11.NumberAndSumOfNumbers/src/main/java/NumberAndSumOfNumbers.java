c0a42f48d819f790c4cea0928e5fa068a9d452b9
Jokieh <1stone.seres@gmail.com>
2022-06-01 18:07:04 +0200

    finished NumberAndSumOfNumbers

--- mooc-java-programming-i/part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -6,5 +6,28 @@ public class NumberAndSumOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Integer numberTimes = 0;
+        Integer sum = 0;
+
+        while (true) {
+
+            System.out.println("Give a number:");
+            Integer number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+
+                break;
+
+            }
+
+            sum = sum + number;
+            numberTimes++;
+
+        }
+
+        System.out.println("Number of numbers: " + numberTimes);
+        System.out.println("Sum of the numbers: " + sum);
+
+
     }
 }
