dbbf169574b96b9f17780ae122ea719831f597ff
Jokieh <1stone.seres@gmail.com>
2022-06-01 18:00:45 +0200

    finished NumberOfNegativeNumbers

--- mooc-java-programming-i/part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
+++ mooc-java-programming-i/part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -6,5 +6,28 @@ public class NumberOfNegativeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Integer negativeTimes = 0;
+
+        while (true) {
+
+            System.out.println("Give a number:");
+            Integer number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+
+                break;
+
+            }
+
+            if (number < 0) {
+
+                negativeTimes++;
+
+            }
+
+        }
+
+        System.out.println("Number of negative numbers: " + negativeTimes);
+
     }
 }
