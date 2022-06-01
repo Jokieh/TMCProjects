ba687e8bc881961dea10f62dd9ba9ec8590ce2e6
Jokieh <1stone.seres@gmail.com>
2022-06-01 17:58:00 +0200

    finished NumberOfNumbers

--- mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -6,5 +6,24 @@ public class NumberOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Integer numberTimes = 0;
+
+        while (true) {
+
+            System.out.println("Give a number:");
+            Integer number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+
+                break;
+
+            }
+
+            numberTimes++;
+
+        }
+
+        System.out.println("Number of numbers: " + numberTimes);
+
     }
 }
