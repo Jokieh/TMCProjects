ad86e6b781987825b4d6543cd78fc5e0ae32730d
Jokieh <1stone.seres@gmail.com>
2022-06-02 14:32:18 +0200

    finished AverageOfNumbers

--- mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -1,4 +1,7 @@
 
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AverageOfNumbers {
@@ -6,5 +9,24 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        List <Integer> numbersList = new ArrayList<>();
+
+        while (true) {
+
+            System.out.println("Give a number");
+            Integer number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+
+                break;
+
+            }
+
+            numbersList.add(number);
+
+
+        }
+
+        System.out.println("Average of the numbers: " + (1.0 * numbersList.stream().mapToInt(Integer::intValue).sum() / numbersList.size()));
     }
 }
