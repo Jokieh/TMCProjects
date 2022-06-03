c616836bcba483eee455a4123fc756c8285abd11
Jokieh <1stone.seres@gmail.com>
2022-06-03 10:38:12 +0200

    finished Counting

--- mooc-java-programming-i/part02-Part02_14.Counting/src/main/java/Counting.java
+++ mooc-java-programming-i/part02-Part02_14.Counting/src/main/java/Counting.java
@@ -6,5 +6,13 @@ public class Counting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Integer number = Integer.valueOf(scanner.nextLine());
+
+        for (int i = 0; i <= number; i++) {
+
+            System.out.println(i);
+
+        }
+
     }
 }
