17f7150605f3ef1d077b30e65cb93b8b81d71a25
Jokieh <1stone.seres@gmail.com>
2022-06-03 11:12:07 +0200

    finished SumOfASequenceTheSequel

--- mooc-java-programming-i/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ mooc-java-programming-i/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -6,5 +6,21 @@ public class SumOfASequenceTheSequel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("First number?");
+        Integer firstNumber = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Last number?");
+        Integer lastNumber = Integer.valueOf(scanner.nextLine());
+
+        Integer sum = 0;
+
+        for (int i = firstNumber; i <= lastNumber; i++) {
+
+            sum = sum + i;
+
+        }
+
+        System.out.println("The sum is " + sum);
+
     }
 }
