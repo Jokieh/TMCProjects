06e34e5256ddf51f967b1e6cd14a7b1659d245cd
Jokieh <1stone.seres@gmail.com>
2022-06-06 16:41:50 +0200

    finished AverageOfAList

--- mooc-java-programming-i/part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
+++ mooc-java-programming-i/part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -1,5 +1,7 @@
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AverageOfAList {
@@ -7,10 +9,26 @@ public class AverageOfAList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // implement here a program, that first reads user input
-        // adding them on a list until user gives -1.
-        // Then it computes the average of the numbers on the list
-        // and prints it.
+        List <Integer> list = new ArrayList<>();
+        Integer sum = 0;
+
+        while (true) {
+
+            Integer number = Integer.valueOf(scanner.nextLine());
+
+            if (number != -1) {
+
+                list.add(number);
+                sum += number;
+
+            } else {
+
+                break;
+
+            }
+        }
+
+        System.out.println("Average " + (1.00 * sum / list.size()));
         
     }
 }
