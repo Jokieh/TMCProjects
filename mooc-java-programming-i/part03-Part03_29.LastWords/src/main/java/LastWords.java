cce6a44b3d48b4b5819c2c239c6732e463a20c60
Jokieh <1stone.seres@gmail.com>
2022-06-11 01:44:46 +0200

    finished LastWords

--- mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LastWords {
@@ -6,6 +8,27 @@ public class LastWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String input = "";
+
+
+        List<String[]> list = new ArrayList<>();
+
+        do {
+
+            input = scanner.nextLine();
+            if (!input.isEmpty()) {
+
+                list.add(input.split(" "));
+
+            }
+
+        } while (!input.isEmpty());
+
+        for (String[] array : list) {
+
+            System.out.println(array[array.length-1]);
+
+        }
 
     }
 }
