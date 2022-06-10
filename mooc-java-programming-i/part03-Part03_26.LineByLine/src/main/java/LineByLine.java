f4d1213e68c2ea57377fbad8ce8e6af31124c658
Jokieh <1stone.seres@gmail.com>
2022-06-11 01:44:54 +0200

    finished LineByLine

--- mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -6,6 +6,23 @@ public class LineByLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String inputString = "";
+        String wholeString = "";
 
+        do {
+
+            inputString = scanner.nextLine();
+            wholeString += (inputString + " ");
+
+        } while (!inputString.isEmpty());
+
+
+        String[] wholeStringArray = wholeString.split(" ");
+
+        for (String word : wholeStringArray) {
+
+            System.out.println(word);
+
+        }
     }
 }
