2b9f371a266938058e97062e2c8a75fdaff9df70
Jokieh <1stone.seres@gmail.com>
2022-06-11 01:45:05 +0200

    finished Login

--- mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
+++ mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
@@ -1,4 +1,6 @@
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Login {
@@ -6,5 +8,27 @@ public class Login {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Map <String,String> userList = new HashMap<>();
+
+        userList.put("alex", "sunshine");
+        userList.put("emma", "haskell");
+
+        System.out.println("Enter username:");
+        String username = scanner.nextLine();
+
+        System.out.println("Enter password:");
+        String password = scanner.nextLine();
+
+        if (userList.containsKey(username) && userList.get(username).equals(password)) {
+
+            System.out.println("You have successfully logged in!");
+
+        } else {
+
+            System.out.println("Incorrect username or password!");
+
+        }
+
     }
+
 }
