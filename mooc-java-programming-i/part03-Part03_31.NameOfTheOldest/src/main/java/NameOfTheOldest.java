308a7b2ffbf4234e083d7f574b44fa98dc1ae4c2
Jokieh <1stone.seres@gmail.com>
2022-06-11 01:45:14 +0200

    finished NameOfTheOldest

--- mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -6,6 +6,31 @@ public class NameOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Integer oldest = 0;
+        String oldestName = "";
+
+        while (true) {
+
+            String input = scanner.nextLine();
+
+            if (input.isEmpty()) {
+
+                break;
+
+            }
+
+            if (Integer.valueOf(input.split(",")[1]) > oldest) {
+
+                oldest = Integer.valueOf(input.split(",")[1]);
+                oldestName = input.split(",")[0];
+
+            }
+        }
+
+        System.out.println("Name of the oldest: " + oldestName);
+
+
+
 
     }
 }
