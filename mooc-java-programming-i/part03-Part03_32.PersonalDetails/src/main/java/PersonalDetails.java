fa5e52328714c8c36cab327f7bb5d3c937d334a2
Jokieh <1stone.seres@gmail.com>
2022-06-11 01:45:22 +0200

    finished PersonalDetails

--- mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,6 +7,35 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String longestName = "";
+        Integer sum = 0;
+        int i = 0;
+
+        while (true) {
+
+            String input = scanner.nextLine();
+
+            if (input.isEmpty()) {
+
+                break;
+
+            }
+
+            i++;
+            sum += Integer.valueOf(input.split(",")[1]);
+
+            if (input.split(",")[0].length() > longestName.length()) {
+
+                longestName = input.split(",")[0];
+
+            }
+        }
+
+        System.out.println("Longest name: " + longestName);
+        System.out.println("Average of the birth years: " + 1.0 * sum / i);
+
+
+
 
     }
 }
