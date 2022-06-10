901118cc450c9ccda0493d7f520400496ffa8e36
Jokieh <1stone.seres@gmail.com>
2022-06-11 01:44:30 +0200

    finished AVClub

--- mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -6,6 +6,25 @@ public class AVClub {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String wholeInput = "";
+        String input = "";
+
+        do {
+
+            input = scanner.nextLine();
+            wholeInput += input + " ";
+
+        } while (!input.isEmpty());
+
+        for (String word : wholeInput.split("\\s")) {
+
+            if (word.contains("av")) {
+
+                System.out.println(word);
+
+            }
+
+        }
 
     }
 }
