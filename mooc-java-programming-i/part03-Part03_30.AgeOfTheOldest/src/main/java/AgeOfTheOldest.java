4e6d24fe46748e961d696ddaad3e7cef880f85b4
Jokieh <1stone.seres@gmail.com>
2022-06-11 01:44:21 +0200

    finished AgeOfTheOldest

--- mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -6,6 +6,27 @@ public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Integer oldest = 0;
+
+        while (true) {
+
+            String input = scanner.nextLine();
+
+            if (input.isEmpty()) {
+
+                break;
+
+            }
+
+            if (Integer.valueOf(input.split(",")[1]) > oldest) {
+
+                oldest = Integer.valueOf(input.split(",")[1]);
+
+            }
+        }
+
+        System.out.println("Age of the oldest: " + oldest);
+
 
     }
 }
