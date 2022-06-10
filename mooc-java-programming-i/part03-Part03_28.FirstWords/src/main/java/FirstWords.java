ccb6ca9d93224c8821ed57000114afffbeb078d3
Jokieh <1stone.seres@gmail.com>
2022-06-11 01:44:39 +0200

    finished FirstWords

--- mooc-java-programming-i/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ mooc-java-programming-i/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -1,10 +1,34 @@
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class FirstWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String input = "";
+        String wholeInput = "";
+
+        List <String[]> list = new ArrayList<>();
+
+        do {
+
+            input = scanner.nextLine();
+            if (!input.isEmpty()) {
+
+                list.add(input.split(" "));
+
+            }
+
+        } while (!input.isEmpty());
+
+       for (String[] array : list) {
+
+           System.out.println(array[0]);
+
+       }
 
 
     }
