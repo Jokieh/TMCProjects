3313a4947e903303e98f00f163565776a4387ecd
Jokieh <1stone.seres@gmail.com>
2022-06-11 03:16:21 +0200

    finished YourFirstAccount

--- mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -2,7 +2,11 @@
 public class YourFirstAccount {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // Write your program here
+
+        Account someAccount = new Account("something", 100.0);
+
+        someAccount.deposit(20.0);
+        System.out.println(someAccount);
+
     }
 }
