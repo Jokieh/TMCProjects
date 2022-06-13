3a273d4baac29f96533bba542f215f17b77945b3
Jokieh <1stone.seres@gmail.com>
2022-06-13 04:19:54 +0200

    finished RecordsFromAFile

--- mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -7,5 +7,21 @@ public class RecordsFromAFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Name of the file:");
+        String file = scanner.nextLine();
+
+        try (Scanner fileScanner = new Scanner(Paths.get(file))) {
+            while (fileScanner.hasNextLine()) {
+                String row = fileScanner.nextLine();
+                String name = row.split(",")[0];
+                Integer age = Integer.valueOf(row.split(",")[1]);
+                System.out.print(name + ", age: " + age + " year");
+                if (age > 1 || age == 0) System.out.print("s");
+                System.out.println();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+
     }
 }
