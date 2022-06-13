1a51e0859dc2ec45da37e022909ea89d7ce20207
Jokieh <1stone.seres@gmail.com>
2022-06-13 04:20:19 +0200

    finished SportStatistics

--- mooc-java-programming-i/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ mooc-java-programming-i/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -1,6 +1,7 @@
 
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class SportStatistics {
@@ -8,6 +9,38 @@ public class SportStatistics {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("File:");
+        String file = scan.nextLine();
+
+        System.out.println("Team:");
+        String team = scan.nextLine();
+
+        Integer games = 0;
+        Integer wins = 0;
+        Integer losses = 0;
+
+        try (Scanner fileScanner = new Scanner(Paths.get(file))){
+            while (fileScanner.hasNextLine()) {
+                String row = fileScanner.nextLine();
+                String team1 = row.split(",")[0];
+                String team2 = row.split(",")[1];
+                Integer team1Score = Integer.valueOf(row.split(",")[2]);
+                Integer team2Score = Integer.valueOf(row.split(",")[3]);
+
+                if (team.equals(team1) || team.equals(team2)) {
+                    games++;
+                    if ((team.equals(team1) && team1Score > team2Score) || (team.equals(team2) && team2Score > team1Score)) {
+                        wins++;
+                    } else losses++;
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+
+        System.out.println("Games: " + games);
+        System.out.println("Wins: " + wins);
+        System.out.println("Losses: " + losses);
     }
 
 }
