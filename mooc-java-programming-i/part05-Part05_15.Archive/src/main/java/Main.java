6a3e3bc1f3616f425033f5be46928bfd440e257b
Jokieh <1stone.seres@gmail.com>
2022-06-22 17:42:56 +0200

    finished Archive

--- mooc-java-programming-i/part05-Part05_15.Archive/src/main/java/Main.java
+++ mooc-java-programming-i/part05-Part05_15.Archive/src/main/java/Main.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +8,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        HashMap <String, String> idNameMap = new HashMap<>();
+
+
+        while (true) {
+            String identifier;
+            String name;
+
+            System.out.println("Identivfier? (empty will stop)");
+            identifier = scanner.nextLine();
+            if (identifier.isEmpty()) break;
+
+            System.out.println("Name? (empty will stop)");
+            name = scanner.nextLine();
+            if (name.isEmpty()) break;
+
+            if (idNameMap.containsKey(identifier)) continue;
+            idNameMap.put(identifier, name);
+
+        }
+
+        for (String element : idNameMap.keySet()) {
+            System.out.println(element + ": " + idNameMap.get(element));
+        }
 
     }
 }
