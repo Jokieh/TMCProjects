1cd9daf7717e3c24e5a2e88b961acbc2a2de0ce5
Jokieh <1stone.seres@gmail.com>
2022-06-22 17:43:51 +0200

    finished Song

--- mooc-java-programming-i/part05-Part05_12.Song/src/main/java/Song.java
+++ mooc-java-programming-i/part05-Part05_12.Song/src/main/java/Song.java
@@ -10,7 +10,12 @@ public class Song {
         this.name = name;
         this.durationInSeconds = durationInSeconds;
     }
-
+    @Override
+    public boolean equals (Object otherSong) {
+        Song other = (Song) otherSong;
+        if (this.artist.equals(other.artist) && this.name.equals(other.name) && this.durationInSeconds == other.durationInSeconds) return true;
+        return false;
+    }
     @Override
     public String toString() {
         return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
